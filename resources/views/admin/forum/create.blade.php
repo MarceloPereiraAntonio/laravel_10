5948a4fafe2f57a89fb68801decb57e7d4414b90
marcelopereira <marcelo.pereira.antonio0@gmail.com>
2024-06-29 14:48:42 -0300

    add app layouts on create and add form style

--- resources/views/admin/forum/create.blade.php
+++ resources/views/admin/forum/create.blade.php
@@ -1,6 +1,13 @@
+@extends('admin.layout.app')
+
+@section('header')
 <h1>Cadastrar nova pergunta</h1>
+@endsection
+@section('content')
+
 <x-alert/>
 <form action="{{route('forum.store')}}" method="post">
     @include('admin.forum.partils.form')
 
 </form>
+@endsection
--- resources/views/admin/forum/partils/form.blade.php
+++ resources/views/admin/forum/partils/form.blade.php
@@ -1,8 +1,19 @@
+<style>
+    input, textarea {
+        background-color: #1817171c !important;
+    }
+</style>
 @csrf
-<label for="subject">Assunto</label>
-<input type="text" name="subject" value="{{$data->subject ?? old('subject')}}">
-<label for="body">
-    Menssagem
-    <textarea name="body"  cols="30" rows="10">{{$data->body ?? old('body')}}</textarea>
-</label>
-<button type="submit">Enviar</button>
+<div class="mb-3">
+    <label for="subject" class="form-label"><span class="badge text-bg-dark">Assunto</span></label>
+    <input type="text" class="form-control" name="subject" value="{{$data->subject ?? old('subject')}}" >
+</div>
+<div class="mb-3">
+    <label for="body" class="form-label"><span class="badge text-bg-dark">Menssagem</span></label>
+    <textarea name="body" class="form-control"  cols="30" rows="10" placeholder="Escreva sua pergunta...">{{$data->body ?? old('body')}}</textarea>
+</div>
+<div class="d-flex">
+    <button class="btn btn-success" type="submit">Enviar</button>
+    <a href="{{redirect()->getUrlGenerator()->previous()}}" type="button" class="btn btn-secondary ms-2" >Voltar</a>
+</div>
+
