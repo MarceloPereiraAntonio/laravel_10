d49b0d35c064039dd6fe03e7af20015c786bda71
marcelopereira <marcelo.pereira.antonio0@gmail.com>
2024-07-09 19:21:23 -0300

    install laravel/ui for autentication and install bootstrap and added middleware in routes

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,24 @@
 
 use App\Enums\ForumStatusEnum;
 use App\Http\Controllers\Admin\{ForumController};
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Route::middleware('auth')->group(function (){
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/forum', [ForumController::class, 'index'])->name('forum.index');
+    Route::get('/forum/create', [ForumController::class, 'create'])->name('forum.create');
+    Route::post('/forum',[ForumController::class, 'store'])->name('forum.store');
+    Route::get('/forum/show/{id}', [ForumController::class, 'show'])->name('forum.show');
+    Route::get('/forum/edit/{id}', [ForumController::class, 'edit'])->name('forum.edit');
+    Route::put('/forum/edit/{id}', [ForumController::class, 'update'])->name('forum.update');
+    Route::delete('/forum/destroy/{id}', [ForumController::class, 'destroy'])->name('forum.destroy');
 
-Route::get('/forum', [ForumController::class, 'index'])->name('forum.index');
-Route::get('/forum/create', [ForumController::class, 'create'])->name('forum.create');
-Route::post('/forum',[ForumController::class, 'store'])->name('forum.store');
-Route::get('/forum/show/{id}', [ForumController::class, 'show'])->name('forum.show');
-Route::get('/forum/edit/{id}', [ForumController::class, 'edit'])->name('forum.edit');
-Route::put('/forum/edit/{id}', [ForumController::class, 'update'])->name('forum.update');
-Route::delete('/forum/destroy/{id}', [ForumController::class, 'destroy'])->name('forum.destroy');
+});
+Auth::routes();
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
